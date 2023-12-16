06923d13ed4afd301efeadc38df08a1794c123dd
VINAYAK-MK <148335561+VINAYAK-MK@users.noreply.github.com>
2023-12-16 12:41:52 +0530

    Update client.py

--- server_client_LAN/client.py
+++ server_client_LAN/client.py
@@ -1,6 +1,9 @@
 import socket
 from threading import Thread
-from time import *
+
+server_IP="IP adress of device in wwhich server code is running and type should be String"
+port="port number on which server is listening and type should be integer"
+
 def input_taking():
 	while True:
 		m=input()
@@ -13,11 +16,11 @@ def recv():
 
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 try:
-	client.connect(("127.0.0.1",8080))
+	client.connect(("server_IP",port))
 except socket.error as a:
 	print("not connected")
 msg=(client.recv(1024)).decode("utf-8")
-t=Thread(target=input_taking)
-t.start()
-thy=Thread(target=recv)
-thy.start()
+take_input=Thread(target=input_taking)
+take_input.start()
+recieve=Thread(target=recv)
+recieve.start()
